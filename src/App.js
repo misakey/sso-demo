bcedc68611180d0941b1773f73ee523537ccb31b
= <antoine.vadot@misakey.com>
2020-06-04 10:31:52 +0200

    feat: first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { useMisakeyAuth } from "@misakey/sdk";
+
+/** Copy this snippet in the config part of your app */
+const authConfig = {
+  clientId: '9e9f68f2-1337-491a-9d04-3e44442aba1b',
+  redirectUri: 'http://localhost:3000/callback',
+  buttonPlacement: 'top-right',
+  userInfoRequirement: ['email', 'name', 'phone'],
+}
+
+function App() {
+  /** Copy this snippet into your root component */
+  const { isAuthCallback, isAuthenticated, userProfile } = useMisakeyAuth(authConfig);
+  if (isAuthCallback) { return null; }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {isAuthenticated ? (
+            `Hello ${userProfile.name}`
+          ) : (
+            'Hello stranger, please signin !'
+          )}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
